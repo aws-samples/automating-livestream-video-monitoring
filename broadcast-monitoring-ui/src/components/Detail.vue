7a4246c125541cb5d1b3c3239eece9c2a1ff26cd
Angela W <angelaw@amazon.com>
2020-10-06 11:03:40 -0400

    fix vuex store access

--- broadcast-monitoring-ui/src/components/Detail.vue
+++ broadcast-monitoring-ui/src/components/Detail.vue
@@ -70,7 +70,7 @@ export default {
     srcUrl: function() {
       if (this.getSegmentDetails) {
         const { Start_DateTime: start, End_DateTime: end } = this.getSegmentDetails
-        const rawUrl = this.$store.streamMasterUrl
+        const rawUrl = this.$store.state.streamMasterUrl
         return start && end ? `${rawUrl}?start=${start}&end=${end}` : rawUrl
       } else {
         return null
